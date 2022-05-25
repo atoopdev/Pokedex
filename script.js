77dc95414dad0d62df0819d25fde655e7eaf41d1
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-05-25 11:06:13 -0700

    basic grid layout

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ async function getPokemon(){
 
 getPokemon().then(pokemon =>{
     console.log(pokemon)
-    document.getElementById("grid").innerHTML = `${pokemon.map(getPokemonHTML).join('')}`
+    document.getElementById("grid").innerHTML = `<div class="pokemon-category"><div>ID</div><div class="">English Name</div><div class="">Type</div><div class="">HP</div><div class="">Attack</div><div class="">Defense</div><div class="">Speed</div></div>
+    ${pokemon.map(getPokemonHTML).join('')}`
 })
 
 function getPokemonHTML(indivPokemon){
-    return `<div>ID:${indivPokemon.id} ${indivPokemon.name.english} Type: ${indivPokemon.type} HP: ${indivPokemon.base.HP} Attack: ${indivPokemon.base.Attack} Defense: ${indivPokemon.base.Defense} Speed: ${indivPokemon.base.Speed}</div>` 
+    return `<div class="pokemon-listing"><div class="id">${indivPokemon.id}</div> <div class="name">${indivPokemon.name.english}</div> <div class="type">${indivPokemon.type}</div> <div class="hp">${indivPokemon.base.HP}</div> <div class="attack">${indivPokemon.base.Attack}</div> <div class="defense">${indivPokemon.base.Defense}</div> <div class="speed">${indivPokemon.base.Speed}</div></div>` 
 }
