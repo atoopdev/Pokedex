1ac6c35ef1a2249d522c69ec6ab294c2a685af14
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-05-25 14:14:26 -0700

    Add additional names and format

--- script.js
+++ script.js
@@ -18,5 +18,9 @@ getPokemon().then(pokemon =>{
 })
 
 function getPokemonHTML(indivPokemon){
-    return `<div class="pokemon-listing"><div class="pokemon-id">${indivPokemon.id}</div> <div class="pokemon-name">${indivPokemon.name.english}</div> <div class="pokemon-type">${indivPokemon.type.join(' / ')}</div> <div class="pokemon-stat">HP: ${indivPokemon.base.HP}</div> <div class="pokemon-stat">Attack: ${indivPokemon.base.Attack}</div> <div class="pokemon-stat">Defense: ${indivPokemon.base.Defense}</div> <div class="pokemon-stat">Speed: ${indivPokemon.base.Speed}</div></div>` 
+    return `<div class="pokemon-listing"><div class="pokemon-id">${indivPokemon.id}</div> <div class="pokemon-name">${indivPokemon.name.english}</div> <div class="pokemon-type">${indivPokemon.type.join(' / ')}</div> <div class="pokemon-stat">HP: ${indivPokemon.base.HP}</div> <div class="pokemon-stat">Attack: ${indivPokemon.base.Attack}</div> <div class="pokemon-stat">Defense: ${indivPokemon.base.Defense}</div> <div class="pokemon-stat">Speed: ${indivPokemon.base.Speed}</div>
+    <div class="pokemon-add-names">${indivPokemon.name.japanese}</div>
+    <div class="pokemon-add-names">${indivPokemon.name.chinese}</div>
+    <div class="pokemon-add-names">${indivPokemon.name.french}</div>
+    </div>` 
 }
